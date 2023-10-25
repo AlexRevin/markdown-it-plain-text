21a097fbf4593cc73170d25f4778385d759f686a
Alex <alex@jaralex.ee>
2023-10-25 19:20:16 +0300

    no new line escape

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = function plainTextPlugin(md) {
   function plainTextRule(state) {
     var text = scan(state.tokens);
     // remove redundant white spaces
-    md.plainText = text.replace(/\s+/g, " ");
+    md.plainText = text.replace(/[^\S\r\n]+/g, " ");
   }
 
   function scan(tokens) {
